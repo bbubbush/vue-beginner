7abf259fabee4b2135c2e24fbfb6fafbe88d561c
bbubbush <bbubbush@gmail.com>
2021-12-20 13:55:07 +0900

    init

---
+++ vue-news/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NewsView from "../views/NewsView.vue";
+import JobsView from "../views/JobsView.vue";
+import AskView from "../views/AskView.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/news'
+        },
+        {
+            path: '/news',
+            component: NewsView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+    ]
+})
+
+export default router;
